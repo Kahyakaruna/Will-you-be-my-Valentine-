24e6eb316b1fec02a1802767ae8a72dec942ac78
Kahyakaruna <kahyakaruna.laksmana@gmail.com>
2026-02-09 23:05:50 +0800

    yes

--- script.js
+++ script.js
@@ -56,13 +56,12 @@
 const messages = [
     "Are you sure?",
     "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
+    "Are you really sure?",
+    "姐姐 please...",
     "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
+    "I will cry...",
+    "I will cry all night...",
+    "Ok fine, you hate me....",
     "Just kidding, say yes please! ❤️"
 ];
 
